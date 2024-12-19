db6904adb9bb16ab36b40e072abe003109c83793
bshafi <62918721+bshafi@users.noreply.github.com>
2024-12-18 18:57:04 -0800

    fix pylit errors

--- leetcode/35_search_insert_position.py
+++ leetcode/35_search_insert_position.py
@@ -5,15 +5,21 @@ hi = 0, lo = 1
 mid = 0
 """
 
+from typing import List
+
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        Find the target in a sorted array. 
+        If the target is not in the array returns the position where it would be inserted
+        """
         lo = 0
         hi = len(nums) - 1
         while lo <= hi:
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
+            if nums[mid] > target:
                 hi = mid - 1
             else:
                 lo = mid + 1
