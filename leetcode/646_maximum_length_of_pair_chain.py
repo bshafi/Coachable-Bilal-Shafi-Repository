eec927fd5b5aae07fa1b2ee0b6ca3311131b96b8
bshafi <62918721+bshafi@users.noreply.github.com>
2025-01-02 21:08:10 -0800

    solved 646

---
+++ leetcode/646_maximum_length_of_pair_chain.py
@@ -0,0 +1,28 @@
+"""
+            Find the longest chain of pairs where the last element of 
+            the previous pair is strictly less than the first element of the next pair.
+"""
+
+from typing import List
+
+class Solution:
+    """
+            Find the longest chain of pairs where the last element of 
+            the previous pair is strictly less than the first element of the next pair.
+    """
+    def find_longest_chain(self, pairs: List[List[int]]) -> int:
+        """
+            Find the longest chain of pairs where the last element of 
+            the previous pair is strictly less than the first element of the next pair.
+        """
+        pairs.sort(key=lambda elem: elem[1])
+        count = 0
+        last_chain = None
+        for start, end in pairs:
+            if last_chain is None:
+                last_chain = end
+                count += 1
+            elif last_chain < start:
+                last_chain = end
+                count += 1
+        return count
