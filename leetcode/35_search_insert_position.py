70653a907dadf43d298b99030dee84eb58aa9ed1
bshafi <62918721+bshafi@users.noreply.github.com>
2025-01-16 12:25:23 -0800

    Fix pylint errors

--- leetcode/35_search_insert_position.py
+++ leetcode/35_search_insert_position.py
@@ -1,26 +1,22 @@
 """
-1 3 5 6
-target = 2
-hi = 0, lo = 1
-mid = 0
+    35. Search Insert Position
 """
 
 from typing import List
 
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        """
-        Find the target in a sorted array. 
-        If the target is not in the array returns the position where it would be inserted
-        """
-        lo = 0
-        hi = len(nums) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[mid] > target:
-                hi = mid - 1
-            else:
-                lo = mid + 1
-        return lo
+def search_insert(nums: List[int], target: int) -> int:
+    """
+    Find the target in a sorted array. 
+    If the target is not in the array returns the position where it would be inserted
+    """
+    lo = 0
+    hi = len(nums) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[mid] > target:
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return lo
--- leetcode/784_letter_case_permutation.py
+++ leetcode/784_letter_case_permutation.py
@@ -1,21 +1,24 @@
+"""
+    784. Letter Case Permutation
+"""
+
 from typing import List
 
-class Solution:
-    def letterCasePermutation(self, s: str) -> List[str]:
-        """
-        Generates all permutations of upper and lowercase english characters.
-        """
-        if len(s) == 0:
-            return []
-        permutations = self.letterCasePermutation(s[1:])
-        c = s[0]
-        letters = [c]
-        if ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z'):
-            letters = [c.upper(), c.lower()]
-        if len(permutations) == 0:
-            return letters
-        ret = []
-        for letter in letters:
-            for permutation in permutations:
-                ret.append(letter + permutation)
-        return ret
+def letter_case_permutation(self, s: str) -> List[str]:
+    """
+    Generates all permutations of upper and lowercase english characters.
+    """
+    if len(s) == 0:
+        return []
+    permutations = self.letterCasePermutation(s[1:])
+    c = s[0]
+    letters = [c]
+    if ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z'):
+        letters = [c.upper(), c.lower()]
+    if len(permutations) == 0:
+        return letters
+    ret = []
+    for letter in letters:
+        for permutation in permutations:
+            ret.append(letter + permutation)
+    return ret
