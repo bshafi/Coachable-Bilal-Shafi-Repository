47ce52c5004baa71305642fec8212380d4319730
bshafi <62918721+bshafi@users.noreply.github.com>
2025-02-27 11:16:42 -0800

    523. Continous Subarray Sum

--- leetcode/523_continous_subarray_sum.py
+++ leetcode/523_continous_subarray_sum.py
@@ -4,11 +4,11 @@ from typing import List
 
 def check_subarray_sum(nums: List[int], k: int) -> bool:
     "Check subarray Sum"
-    remainder_cache = {0:-1}
+    remainder_cache = { 0:-1 }
     remainder = 0
     for i, num in enumerate(nums):
         remainder += num
-        remainder %=k
+        remainder %= k
         if remainder not in remainder_cache:
             remainder_cache[remainder] = i
         elif i - remainder_cache[remainder] >=2:
